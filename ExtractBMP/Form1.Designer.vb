6aeeb42e87d05362f356faefc8a7cc7a016b9562
AlliedHammer <allenhurtig@gmail.com>
2024-12-16 10:21:54 -0500

    Add files via upload

---
+++ ExtractBMP/Form1.Designer.vb
@@ -0,0 +1,136 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ExtractBMP
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(ExtractBMP))
+        Me.InputTextBox = New System.Windows.Forms.TextBox()
+        Me.OutputTextBox = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.InputButton = New System.Windows.Forms.Button()
+        Me.OutputButton = New System.Windows.Forms.Button()
+        Me.ConvertButton = New System.Windows.Forms.Button()
+        Me.InfoBox = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'InputTextBox
+        '
+        Me.InputTextBox.Location = New System.Drawing.Point(96, 39)
+        Me.InputTextBox.Name = "InputTextBox"
+        Me.InputTextBox.Size = New System.Drawing.Size(563, 20)
+        Me.InputTextBox.TabIndex = 0
+        '
+        'OutputTextBox
+        '
+        Me.OutputTextBox.Location = New System.Drawing.Point(96, 88)
+        Me.OutputTextBox.Name = "OutputTextBox"
+        Me.OutputTextBox.Size = New System.Drawing.Size(563, 20)
+        Me.OutputTextBox.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 42)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(51, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = ".DAT File"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 91)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(78, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = ".BMP Directory"
+        '
+        'InputButton
+        '
+        Me.InputButton.Location = New System.Drawing.Point(665, 37)
+        Me.InputButton.Name = "InputButton"
+        Me.InputButton.Size = New System.Drawing.Size(75, 23)
+        Me.InputButton.TabIndex = 4
+        Me.InputButton.Text = "Browse"
+        Me.InputButton.UseVisualStyleBackColor = True
+        '
+        'OutputButton
+        '
+        Me.OutputButton.Location = New System.Drawing.Point(665, 86)
+        Me.OutputButton.Name = "OutputButton"
+        Me.OutputButton.Size = New System.Drawing.Size(75, 23)
+        Me.OutputButton.TabIndex = 5
+        Me.OutputButton.Text = "Browse"
+        Me.OutputButton.UseVisualStyleBackColor = True
+        '
+        'ConvertButton
+        '
+        Me.ConvertButton.Location = New System.Drawing.Point(339, 120)
+        Me.ConvertButton.Name = "ConvertButton"
+        Me.ConvertButton.Size = New System.Drawing.Size(75, 23)
+        Me.ConvertButton.TabIndex = 6
+        Me.ConvertButton.Text = "Extract"
+        Me.ConvertButton.UseVisualStyleBackColor = True
+        '
+        'InfoBox
+        '
+        Me.InfoBox.Location = New System.Drawing.Point(12, 189)
+        Me.InfoBox.Multiline = True
+        Me.InfoBox.Name = "InfoBox"
+        Me.InfoBox.ReadOnly = True
+        Me.InfoBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.InfoBox.Size = New System.Drawing.Size(720, 475)
+        Me.InfoBox.TabIndex = 7
+        '
+        'ExtractBMP
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(744, 676)
+        Me.Controls.Add(Me.InfoBox)
+        Me.Controls.Add(Me.ConvertButton)
+        Me.Controls.Add(Me.OutputButton)
+        Me.Controls.Add(Me.InputButton)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.OutputTextBox)
+        Me.Controls.Add(Me.InputTextBox)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "ExtractBMP"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "ExtractBMP v0.1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+End Sub
+    Friend WithEvents InputTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents OutputTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents InputButton As System.Windows.Forms.Button
+    Friend WithEvents OutputButton As System.Windows.Forms.Button
+    Friend WithEvents ConvertButton As System.Windows.Forms.Button
+    Friend WithEvents InfoBox As System.Windows.Forms.TextBox
+
+End Class
---
+++ ExtractBMP/Form1.vb
@@ -0,0 +1,313 @@
+﻿'|--------------------------------------------------------------------------|
+'|--------------------------------------------------------------------------|
+'|-Project: ExtractBMP   ---------------------------------------------------|
+'|-Programmer: Allen Hurtig   ----------------------------------------------|
+'|-Incept Date: 11/12/2018   -----------------------------------------------|
+'|-Purpose: Extract BMP files from a DAT file.   ---------------------------|
+'|--------------------------------------------------------------------------|
+'|-------------------------Revision History---------------------------------|
+'|-REV 0.1 11/12/2018 :         --------------------------------------------|
+'|--Created program to do extraction of multiple BMP files from a single    |
+'|---.dat file.     --------------------------------------------------------|
+'|------------------------END Revision History------------------------------|
+'|--------------------------------------------------------------------------|
+'|--------------------------------------------------------------------------|
+
+Imports System
+Imports System.Text
+Imports System.IO
+Imports System.IO.Path
+
+Public Class ExtractBMP
+'Form's default size is 760 x 220.
+'Form's converted size is 760 x 715.
+Dim loadSize As New Size(760, 220)
+Dim convertSize As New Size(760, 715)
+Dim bmpList As New List(Of List(Of Byte))
+'Dim byteBitmapList As New List(Of Byte)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'Size the form on loading.
+        HideInfoBox()
+
+    End Sub
+
+    Private Sub InputButton_Click(sender As Object, e As EventArgs) Handles InputButton.Click
+
+        'Load in the .dat file.
+        Dim fileDiag As OpenFileDialog = New OpenFileDialog()
+        Dim fileName As String
+
+        'Setup the file dialog window.
+        fileDiag.Title = "Select a valid DAT file."
+        fileDiag.InitialDirectory = "C:\"
+        fileDiag.Filter = "DAT Files (*.dat)|*.dat"
+        fileDiag.FilterIndex = 1
+        fileDiag.RestoreDirectory = True
+
+        'Check if there is an existing path or not.
+        If InputTextBox.Text <> Nothing Then
+
+            fileDiag.InitialDirectory = GetDirectoryName(InputTextBox.Text.ToString)
+
+        End If
+
+        'Check for a valid file.
+        If fileDiag.ShowDialog() = Windows.Forms.DialogResult.OK Then
+
+            'File was found.
+            fileName = fileDiag.FileName
+
+            'Clear input box and set its text to equal the file path.
+            InputTextBox.Text = fileName.ToString()
+
+            'Setup the output file path.
+            With OutputTextBox
+
+                .Clear()
+                .Text = GetDirectoryName(fileName) & "\" & GetFileNameWithoutExtension(fileName) & ".bmp"
+
+            End With
+
+        End If
+
+        'Hide the info box.
+        HideInfoBox()
+
+    End Sub
+
+    Private Sub OutputButton_Click(sender As Object, e As EventArgs) Handles OutputButton.Click
+
+        'Browse for the location to save the BMP file.
+        Dim fileDiag As SaveFileDialog = New SaveFileDialog()
+        Dim fileName As String
+
+        'Setup the file dialog window.
+        fileDiag.Title = "Select a location to save the DAT file."
+        fileDiag.InitialDirectory = "C:\"
+        fileDiag.Filter = "BMP File (*.bmp)|*.bmp"
+        fileDiag.FilterIndex = 1
+        fileDiag.RestoreDirectory = True
+        fileDiag.FileName = GetFileNameWithoutExtension(InputTextBox.Text.ToString) & ".bmp"
+
+        'Check if there is an existing path or not.
+        If OutputTextBox.Text <> Nothing Then
+
+            fileDiag.InitialDirectory = GetDirectoryName(OutputTextBox.Text.ToString)
+
+        End If
+
+        'Check for a valid file.
+        If fileDiag.ShowDialog() = Windows.Forms.DialogResult.OK Then
+
+            'File was found.
+            fileName = fileDiag.FileName
+
+            'Clear input box and set its text to equal the file path.
+            OutputTextBox.Text = fileName.ToString()
+
+        End If
+
+        'Hide the info box.
+        HideInfoBox()
+
+    End Sub
+
+    Private Sub ConvertButton_Click(sender As Object, e As EventArgs) Handles ConvertButton.Click
+
+        'Show the info box.
+        ShowInfoBox()
+
+        'Clear the info box.
+        InfoBox.Clear()
+
+        'Read in the dat file and extract any bitmaps.
+        GetDATFileData(InputTextBox.Text)
+
+        'Write the bitmaps.
+        WriteBMPFile(OutputTextBox.Text)
+
+        'Clear any data that is left over at this point.
+        bmpList.Clear()
+
+    End Sub
+
+    Private Sub GetDATFileData(ByVal inputFilePath As String)
+
+        'Show the current status in the info box.
+        InfoBox.Text = InfoBox.Text & "-------------------------BEGIN DAT FILE READ-------------------------" & vbCrLf
+
+        'Open the specified DAT file and collect the number of attributes.
+        Try
+
+            Dim binaryReader As New BinaryReader(File.Open(inputFilePath, FileMode.Open))
+            'If the file is not good.
+            If Not File.Exists(inputFilePath) Then
+
+                'Display error message.
+                Dim badFileDiag As DialogResult = MessageBox.Show("Bad or no file detected!", _
+                                                                  "Error!", _
+                                                                  MessageBoxButtons.OK)
+            Else
+
+                'Read in the file.
+
+                'Setup a byte list.
+                Dim byteList As New List(Of Byte)
+                Dim BMPHeader As New List(Of Byte)
+                Dim foundBitmap As Boolean = False
+                Dim bitmapCount As Integer = 0
+                'Dim debugLimit As Integer = 0
+
+                ''Setup a new DO WHILE loop.
+                Do While binaryReader.BaseStream.Position <> binaryReader.BaseStream.Length
+                ' ''Do While debugLimit < 1
+
+                    'Read in the next value.
+                    Dim nextByte As Byte = binaryReader.ReadByte
+
+                    'Add the byte to the list if a bitmap was already found.
+                    If foundBitmap = True Then
+
+                        'Add the byte.
+                        byteList.Add(nextByte)
+                        'byteBitmapList.Add(nextByte)
+
+                    End If
+
+                    'Check if there is a found bitmap.
+                    If nextByte = 66 And BMPHeader.Count = 0 Then
+
+                        'Add the byte to the header.
+                        BMPHeader.Add(nextByte)
+
+                    'Check if there is a second value in the header found.
+                    ElseIf nextByte = 77 And BMPHeader.Count = 1 Then
+
+                        'Add the byte to the header.
+                        BMPHeader.Add(nextByte)
+
+                    'Check if there is a third value in the header found.
+                    ElseIf nextByte = 112 And BMPHeader.Count = 2 Then
+
+                        'Add the byte to the header.
+                        BMPHeader.Add(nextByte)
+
+                    'Check if there is a fourth value in the header found.
+                    ElseIf nextByte = 49 And BMPHeader.Count = 3 Then
+
+                        'Add the byte to the header.
+                        BMPHeader.Add(nextByte)
+
+                        'Do this if a previous bitmap was found, otherwise this is the first bitmap found.
+                        If foundBitmap = True Then
+
+                            'Add the list.
+                            Dim tempList As New List(Of Byte)
+
+                            'Copy the list.
+                            For i As Integer = 0 To byteList.Count - 5 Step 1
+
+                                tempList.Add(byteList.Item(i))
+
+                            Next
+
+                            bmpList.Add(tempList)
+
+                            'Clear the list.
+                            byteList.Clear()
+                            'byteBitmapList.Clear()
+
+                        End If
+
+                        'Add the header.
+                        For Each headerByte As Byte In BMPHeader
+
+                            'Add the header to the list.
+                            byteList.Add(headerByte)
+                            'byteBitmapList.Add(headerByte)
+
+                        Next
+
+                        'Clear the header.
+                        BMPHeader.Clear()
+
+                        'Set the bitmap flag.
+                        foundBitmap = True
+
+                        'debugLimit += 1
+
+                    'Clear the header if the consecutive path isn't followed.
+                    Else
+
+                        BMPHeader.Clear()
+
+                    End If
+
+                Loop
+
+                'Close the file.
+                binaryReader.Close()
+
+            End If
+
+            'Display how many bitmaps were found.
+            InfoBox.Text = InfoBox.Text & bmpList.Count.ToString & " bitmap images found!" & vbCrLf
+
+        Catch ex As Exception
+
+            InfoBox.Text = InfoBox.Text & ex.ToString & vbCrLf
+
+        End Try
+
+    End Sub
+
+    Private Sub WriteBMPFile(ByVal outputFilePath As String)
+
+        'Show the current process in the info box.
+        InfoBox.Text = InfoBox.Text & "-------------------------BEGIN BMP FILE WRITE-------------------------" & vbCrLf
+
+        'Setup the correct output path for each bitmap.
+        Dim baseBMPFilePath As String = outputFilePath.Substring(0, outputFilePath.Length - 4)
+
+            'Begin writing the file.
+            For i As Integer = 0 To bmpList.Count - 1 Step 1
+
+                'Create the new bitmap.
+                Dim BMPFileWriter As New BinaryWriter(File.Open(baseBMPFilePath & i.ToString & ".bmp", FileMode.OpenOrCreate))
+
+                'Get the bytes and write them.
+                For j As Integer = 0 To bmpList(i).Count - 1 Step 1
+
+                    'Write the byte.
+                    BMPFileWriter.Write(bmpList(i).Item(j))
+
+                Next
+
+                'Close the file.
+                BMPFileWriter.Close()
+
+                'Show the written file path.
+                InfoBox.Text = InfoBox.Text & baseBMPFilePath & i.ToString & ".bmp" & vbCrLf
+
+            Next
+
+    End Sub
+
+    Private Sub HideInfoBox()
+
+        'Clear and hide the info box.
+        InfoBox.Clear()
+        Me.Size = loadSize
+
+    End Sub
+
+    Private Sub ShowInfoBox()
+
+        'Show the info box and file info.
+        Me.Size = convertSize
+
+    End Sub
+
+End Class
